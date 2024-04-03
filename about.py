03ea9c75122bf49c868bc60fe2d4af8c3911380a
welly <eng.wellymota@gmail.com>
2024-04-03 00:16:21 -0300

    Database Screen added (As a test only) needs to configure a way to save the data, otherwise it needs to be inserted every time recycle the software.

--- about.py
+++ about.py
@@ -6,7 +6,7 @@ def about():
     janela_about.title("Sobre este Programa")
     janela_about.geometry("480x240")
 
-    label = tk.Label(janela_about, text="Olá, este programa foi gerado por Wellyson Mota")
+    label = tk.Label(janela_about, text="Olá, este programa foi desenvolvido por Welly Mota")
     label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
     label = tk.Label(janela_about, text="Version: 1.0")
